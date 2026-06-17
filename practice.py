e25c8dc23a9025c479707366bd3a8c5e50b569d9
Aarush Reddy <aarushmreddy005@gmail.com>
2026-06-17 12:33:45 +0530

    Edited a sentence

--- practice.py
+++ practice.py
@@ -4,11 +4,11 @@ import shutil
 path = input("Enter Path: ")
 files = os.listdir(path)
 
-for file in files: 
-    filename,extension = os.path.splitext(file)
+for file in files:
+    filename,extension = os.path.splitext(path+'/'+extension)
     extension = extension[1:]
 
-    if os.path.exists(path+"/"+extension):
+    if os.path.exists(path+'/'+extension):
         shutil.move(path+'/'+file, path+'/'+extension+'/'+file)
     else:
         os.makedirs(path+'/'+extension)
